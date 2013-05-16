65e3852738c6c48d070f29404d3bafef828fadbf
Sanjeev Kumar(sanjeevonline) <sanjeevonline@gmail.com>
2013-05-16 14:58:38 +0530

    travel blog added

---
+++ cleanup-site.sh
@@ -0,0 +1,2 @@
+rm -rf tag/ assets/ articles/
+rm tags.html tag.html sitemap.xml rss.xml robots.txt index.json.backup index.html.1 index.html feed.xml blog.html atom.xml archive.html about-me.json.1
