e75b7db94c88fad2fc87c881ce6a81c5972f4851
Sanjeev Kumar <Sanjeev_kumar-xft@mckinsey.com>
2014-10-14 22:31:10 +0530

    updated blog to Wintersmith 2.0

--- cleanup-site.sh
+++ cleanup-site.sh
@@ -1,2 +1,2 @@
-rm -rf tag/ assets/ articles/
+rm -rf tag/ assets/ articles/ src/
 rm tags.html tag.html sitemap.xml rss.xml robots.txt index.json.backup index.html.1 index.html feed.xml blog.html atom.xml archive.html about-me.json.1
